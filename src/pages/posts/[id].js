aea2aad9a7922e493884c7505fcc2a37dfa97f45
hjoo830 <hjoo830@naver.com>
2024-12-01 02:36:12 +0900

    ✨ Feat: Add post detail page

---
+++ src/pages/posts/[id].js
@@ -0,0 +1,39 @@
+import { getAllPostsIds, getPostData } from "@/utils/posts";
+import Head from "next/head";
+import React from "react";
+
+const PostsDetailPage = ({ postData }) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
+};
+
+export default PostsDetailPage;
+
+export const getStaticPaths = async () => {
+  const paths = getAllPostsIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
